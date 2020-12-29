445824d457f7f8af34f66be933bec53c8d57cb91
monilpshah <monil31199@gmail.com>
2020-12-29 22:52:49 +0530

    Auto Complete with Bold and Italic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { EditorChangeContent, EditorChangeSelection, QuillEditorBase, QuillModule } from 'ngx-quill'
-
 import 'quill-emoji/dist/quill-emoji.js'
 import "quill-mention";
 
@@ -15,14 +13,17 @@ import "quill-mention";
   styleUrls: ['app.component.scss']
 })
 
-
 export class AppComponent {
   modules = {};
   content: string = '';
   Optionflag: boolean = false;
   matches = [];
-  blured = false
-  focused = false
+  searchTerm: string = '';
+  bold: boolean = false;
+  italic: boolean = false;
+  blured = false;
+  focused = false;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -32,7 +33,7 @@ export class AppComponent {
 
     // TO ADD FUNCTIONALITY OF EMOJI UNCOMMENT BELOW CODE AND COMMENT / FUNCTIONALITY CODE
 
-    
+
 
     // this.modules = {
     //   'emoji-shortname': true,
@@ -62,11 +63,15 @@ export class AppComponent {
 
     //   ]
     // }
-    this.modules= {
+
+
+    this.modules = {
+      // UNCOMMENT BELOW CODE TO ENABLE AUTO COMPLETE FEATURE
+
       mention: {
         allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
         mentionDenotationChars: ["@", "#", "/"],
-        source: async function(searchTerm, renderList) {
+        source: async function (searchTerm, renderList) {
           const matchedPeople = await suggestPeople(searchTerm);
           renderList(matchedPeople);
         }
@@ -102,12 +107,11 @@ export class AppComponent {
   }
   addBindingCreated(quill) {
     quill.keyboard.addBinding({
-      key: 192
+      key: 192,
     }, (range, context) => {
       // tslint:disable-next-line:no-console
       console.log('KEYBINDING /', range, context)
       this.Optionflag = true;
-      console.log(this.Optionflag);
     })
 
     quill.keyboard.addBinding({
@@ -121,16 +125,48 @@ export class AppComponent {
     })
   }
 
+  checkPin($event: KeyboardEvent) {
+    // console.log($event);
+    // console.log(this.searchTerm);
+    if ((this.bold)==false) {
+      if (this.searchTerm.includes('<span class="ql-mention-denotation-char">/</span>BOLD')){
+        // if(!document.getElementsByClassName('ql-bold ql-active')[0]){
+          if (this.searchTerm.includes('<span class="ql-mention-denotation-char">/</span>ITALIC')) {
+            let italicElement: HTMLElement = document.getElementsByClassName('ql-italic')[0] as HTMLElement;
+            italicElement.click();
+            this.italic = true;
+          }
+        let boldElement: HTMLElement = document.getElementsByClassName('ql-bold')[0] as HTMLElement;
+        boldElement.click();
+        this.bold = true;
+        // this.searchTerm = this.searchTerm.replace('<span class="mention" data-index="0" data-denotation-char="/" data-id="1" data-value="BOLD">﻿<span contenteditable="false"><span class="ql-mention-denotation-char">/</span>BOLD</span>﻿</span>', '');
+        // }
+      }
+    }
+    if (!(this.italic)) {
+      if (this.searchTerm.includes('<span class="ql-mention-denotation-char">/</span>ITALIC')) {
+        if (this.searchTerm.includes('<span class="ql-mention-denotation-char">/</span>BOLD')){
+          let boldElement: HTMLElement = document.getElementsByClassName('ql-bold')[0] as HTMLElement;
+          boldElement.click();
+          this.bold = true;
+        }
+        let italicElement: HTMLElement = document.getElementsByClassName('ql-italic')[0] as HTMLElement;
+        italicElement.click();
+        this.italic = true;
+      }
+    }
+  }
+
 }
 async function suggestPeople(searchTerm) {
   const allPeople = [
     {
       id: 1,
-      value: "Fredrik Sundqvist"
+      value: "BOLD"
     },
     {
       id: 2,
-      value: "Patrik Sjölin"
+      value: "ITALIC"
     }
   ];
   return allPeople.filter(person => person.value.includes(searchTerm));
